250dfa3514c2309a3835d4b839a3af6517ef9d2c
acehrls <chailton@ext.grupo-artico.com>
2022-11-28 12:36:23 -0300

    [FEAT] Incluído página de projetos

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,11 +1,29 @@
 <template>
     <header>
-        <h1>
-            <img src="../assets/logo.png" alt="" />
-        </h1>
-        <button class="button" @click="alterarTema">
-            {{ textoBotao }}
-        </button>
+        <div style="text-align: center">
+            <h1>
+                <img src="../assets/logo.png" alt="" />
+            </h1>
+            <button class="button" @click="alterarTema">
+                {{ textoBotao  }}
+            </button>
+        </div>
+        <div class="panel mt-5">
+            <ul>
+                <li>
+                    <router-link to="/" class="link">
+                        <i class="fas fa-tasks"></i>
+                        tarefas
+                    </router-link>
+                </li>
+                <li>
+                    <router-link to="/projetos" class="link">
+                        <i class="fas fa-project-diagram"></i>
+                        projetos
+                    </router-link>
+                </li>
+            </ul>
+        </div>
     </header>
 </template>
 
@@ -45,7 +63,7 @@ header {
     background: #0d3b66;
     width: 100%;
     height: 100vh;
-    text-align: center;
+
 }
 
 @media only screen and (max-width: 768px) {
@@ -53,5 +71,21 @@ header {
         padding: 2.5rem;
         height: auto;
     }
+}
+
+.panel li {
+    margin: 8px 0;
+}
+
+.link {
+    color: #FFF;
+}
+
+.link:hover {
+    color: #FAF0CA;
+}
+
+.link.router-link-active {
+    color: #FAF0CA;
 }
 </style>
---
+++ src/views/Projetos.vue
@@ -0,0 +1,70 @@
+<template>
+    <section class="projetos">
+        <div class="title">Projetos</div>
+        <form @submit.prevent="salvar">
+            <div class="field">
+                <label for="nomeDoProjeto" class="label">
+                    Nome do Projeto
+                </label>
+                <input type="text" class="input" id="nomeDoProjeto" v-model="nomeDoProjeto">
+            </div>
+            <div class="field">
+                <button class="button" type="submit">
+                    Salvar
+                </button>
+            </div>
+        </form>
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <th>
+                        ID
+                    </th>
+                    <th>
+                        Nome do Projeto
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="projeto in projetos" :key="projeto.id">
+                    <td>{{ projeto.id }}</td>
+                    <td>{{ projeto.nome }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import IProjeto from '../interfaces/IProjeto';
+
+export default defineComponent({
+    name: 'Projetos',
+    data () {
+        return {
+            nomeDoProjeto: '',
+            projetos: [] as IProjeto[]
+        }
+    },
+
+    methods: {
+        salvar() {
+            const projeto: IProjeto = {
+                nome: this.nomeDoProjeto,
+                id: new Date().toISOString()
+            }
+
+            this.projetos.push(projeto);
+            this.nomeDoProjeto = '';
+        }
+    }
+});
+
+</script>
+
+<style scoped>
+.projetos {
+    padding: 1.25rem;
+}
+</style>
